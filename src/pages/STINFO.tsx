c499d8280953a53b53cdc86b7f2e7c8ca4b8f593
vnguyen1412 <saikyouraikiri@gmail.com>
2024-04-18 23:07:30 -0400

    save game data
    have implemented the logic to save the game's data to the database.

--- src/pages/STINFO.tsx
+++ src/pages/STINFO.tsx
@@ -12,10 +12,78 @@ import { Timestamp } from "firebase/firestore";
 function STINFO(){
     const buildContext = GetBuildContext("STINFO");
 
+    function handleSceneLoaded(){
+        LoadUnityProgress();
+    }
 
+    async function LoadUnityProgress(){
+        const email = GetActiveUserEmail();
+        const data = await GetUserData(email);
+
+        buildContext.sendMessage("Game Manager", "LoadGameAttempt", data.STINFOAttemptsLeft);
+        buildContext.sendMessage("Game Manager", "LoadUserAnswers", data.STINFOUserAnsResult);
+        buildContext.sendMessage("Game Manager", "LoadButtonIndex", data.STINFOButtonAnswerChoice);
+        buildContext.sendMessage("Game Manager", "LoadCurrentScreen", data.STINFOCurrentScreen);
+        buildContext.sendMessage("Game Manager", "LoadTrainingStatus", data.STINFOTrainingCompleted);
+    }
+
+    // Bind Unity SceneLoaded event to -> HandleSceneLoaded() above
+    useEffect(() => {
+        buildContext.addEventListener("LoadGameFromDatabase", handleSceneLoaded);
+        return () => {
+            buildContext.removeEventListener("LoadGameFromDatabase", handleSceneLoaded);
+        };
+    }, [buildContext.addEventListener, buildContext.removeEventListener, handleSceneLoaded]);
+    
+    // Handle saving data to the database
+    const  handleSaveGame = useCallback((attemptsLeft: any, userAnsResult: any, buttonAnswerChoice: any, currentScreen: any, trainingCompleted: any) => {
+    //const  handleSaveGame = useCallback((attemptsLeft: any, userAnsResult: any, buttonAnswerChoice: any, trainingCompleted: any) => {
+
+        UpdateSavedData(attemptsLeft, userAnsResult, buttonAnswerChoice, currentScreen, trainingCompleted);
+        //UpdateSavedData(attemptsLeft, userAnsResult, buttonAnswerChoice, trainingCompleted);
+
+    }, []);
+
+    async function UpdateSavedData(attemptsLeft: number, userAnsResult: boolean[], buttonAnswerChoice: string[], currentScreen: number, trainingCompleted: number){
+    //async function UpdateSavedData(attemptsLeft: number, userAnsResult: boolean[], buttonAnswerChoice: string[], trainingCompleted: number){    
+        // Get Email
+        const email = GetActiveUserEmail();
+        const data = await GetUserData(email);
+
+        data["STINFOAttemptsLeft"] = attemptsLeft;
+        data["STINFOUserAnsResult"] = userAnsResult;
+        data["STINFOButtonAnswerChoice"] = buttonAnswerChoice;
+        data["STINFOCurrentScreen"] = currentScreen;
+        data["STINFOTrainingCompleted"] = trainingCompleted;
+        if(trainingCompleted === 1) {
+            data["stinfoCompletionTime"] = Timestamp.now();
+        }
+
+        console.log("STINFOAttemptsLeft has updated to: " + attemptsLeft);
+        console.log("STINFOAttemptsLeft type is: " + typeof attemptsLeft);
+
+        console.log("STINFOUserAnsResult has updated to: " + userAnsResult);
+        console.log("STINFOUserAnsResult type is : " + typeof userAnsResult);
+
+        console.log("STINFOButtonAnswerChoice has updated to: " + buttonAnswerChoice);
+        console.log("STINFOButtonAnswerChoice type is : " + typeof buttonAnswerChoice);
+
+        console.log("STINFOTrainingCompleted has updated to : " + trainingCompleted);
+        console.log("STINFOTrainingCompleted type is : " + typeof trainingCompleted);
+
+        // Save Doc
+        SetDoc(data, "users/" + email);
+    }
+
+    useEffect(() => {
+        buildContext.addEventListener("SaveGameToDatabase", handleSaveGame);
+        return () => {
+            buildContext.removeEventListener("SaveGameToDatabase", handleSaveGame);
+        };
+    }, [buildContext.addEventListener, buildContext.removeEventListener, handleSaveGame]);
     
     return <>
-    <Unity unityProvider={buildContext.unityProvider} className="UnityGame"/>
+        <Unity unityProvider={buildContext.unityProvider} className="UnityGame"/>
     </>;
 }
 
